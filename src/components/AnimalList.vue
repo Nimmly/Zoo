2a09e57c672d3e951fc24970a4cdae883817ea94
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-04-24 18:33:27 +0200

    first

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    
+    <table>
+      <thead>
+        <th>Species</th>
+        <th>Name</th>
+        <th>Day of Birth</th>
+      </thead>
+      <tbody>
+        <tr v-for= "(animal, key) in animals" :key="key">
+          <td>{{ animal.species }}</td>
+          <td>{{ animal.name }}</td>
+          <td>{{ animal.birth }}</td>
+        </tr>
+      </tbody>
+    </table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data() {
+    return { animals:[
+    { species: 'pas', name:'rex', birth:'1.1.2017.'},
+    { species: 'macka', name:'kitty', birth:'1.2.2017.'},
+    { species: 'papagaj', name:'koki', birth:'1.3.2017.'},
+    { species: 'zmija', name:'paja', birth:'1.4.2017.'},
+    { species: 'zec', name:'flafi', birth:'1.5.2017.'}
+
+    ]}}
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+/* vrstu, ime i datum rođenja */
